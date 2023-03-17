c79102026030cbdf5ae768891643329f5f06cbcf
Nui369 <gfamilyno1@gmail.com>
2023-03-17 10:31:07 +0700

    ticked123

--- test-maven/src/main/java/com/example/testmaven/HelloServlet.java
+++ test-maven/src/main/java/com/example/testmaven/HelloServlet.java
@@ -12,13 +12,15 @@ public class HelloServlet extends HttpServlet {
         message = "tao là núi";
     }
 
+
+
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + message + "sơn đẹp trai" + "</h1>");
         out.println("</body></html>");
     }
 

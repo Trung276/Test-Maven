f77626a9923281a7739c3c785536aee4f067fde5
Dat Thanh <thanhnd15032002@gmail.com>
2023-03-17 10:27:09 +0700

    thanh dep trai

--- test-maven/src/main/java/com/example/testmaven/HelloServlet.java
+++ test-maven/src/main/java/com/example/testmaven/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "tao là núi";
+        message = "thanh dep trai";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {

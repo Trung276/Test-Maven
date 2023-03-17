ddbabadbf1e32671f0f747587e7cd2fd135e9198
Trung276 <thanhtrung27061996@gmail.com>
2023-03-17 10:50:19 +0700

    ticket1 change file test-maven/src/main/java/com/example/testmaven/HelloServlet.java

--- test-maven/src/main/java/com/example/testmaven/HelloServlet.java
+++ test-maven/src/main/java/com/example/testmaven/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "tao là núi";
+        message = "tao là núi, khong phai dau";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
